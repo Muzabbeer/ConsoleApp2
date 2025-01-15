1fbd529a171d1db6cd10c18d5cc941923fb18714
Muzabbeer <Muzabbeer@LAPTOP-ITUR1S1O>
2025-01-14 20:49:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var orderSystem = new OrderSystem();
+            var orderHandler = new OrderHandler();
+
+            orderSystem.OrderPlaced += orderHandler.OnOrderPlaced;
+
+            orderSystem.PlaceOrder(12345);
+        }
+    }
+
+    
+    public class OrderEventArgs : EventArgs
+    {
+        public int OrderId { get; set; }
+
+        public OrderEventArgs(int orderId)
+        {
+            OrderId = orderId;
+        }
+    }
+
+   
+    public delegate void OrderPlacedEventHandler(object sender, OrderEventArgs e);
+
+    
+    public class OrderSystem
+    {
+        
+        public event OrderPlacedEventHandler OrderPlaced;
+
+       
+        public void PlaceOrder(int orderId)
+        {
+           
+            OnOrderPlaced(new OrderEventArgs(orderId));
+        }
+
+        
+        protected virtual void OnOrderPlaced(OrderEventArgs e)
+        {
+            
+            OrderPlaced?.Invoke(this, e);
+        }
+    }
+
+    
+    public class OrderHandler
+    {
+        public void OnOrderPlaced(object sender, OrderEventArgs e)
+        {
+            Console.WriteLine($"Order with ID {e.OrderId} has been placed.");
+        }
+    }
+
+}
